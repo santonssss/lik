808905d4f9a7af24b3d38eca7abf746d5d556d9c
santonssss <107553175+santonssss@users.noreply.github.com>
2023-01-17 22:32:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let a,b;
+let c;
+a = prompt(String("Проверка сайта, сколько будет 1+1?"));
+b = prompt(String("Думаешь правильно ответила?"));
+if(a==2){
+    console.log(alert("ой, ошиблась! будет 3"));
+    console.log(alert("Учи английскии!"));
+    console.log(alert("Понела?"));
+    c = prompt(String("Так ты понела или нет?"))
+
+}if(c==String("нет")&&String("Нет")){
+    console.log(alert("так пойми)"))
+}else{
+    console.log(alert("ну ладно)"));
+}
